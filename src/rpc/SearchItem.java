5c739d327b9ccf08c7dfa1c2ceb3fce274fe6a81
William Zhang <wzhang476@gatech.edu>
2018-07-11 01:20:45 -0400

    wzhang: add fav get

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -3,6 +3,7 @@ package rpc;
 import java.io.IOException;
 
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import db.*;
 import entity.Item;
@@ -37,14 +39,18 @@ public class SearchItem extends HttpServlet {
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		String keyword = request.getParameter("term");
+		String userId = request.getParameter("user_id");
 		
 		DBConnection connection = DBConnectionFactory.getConnection();
 		try {
 			List<Item> items = connection.searchItems(lat, lon, keyword);
-			
+			Set<String> favoriteItems = connection.getFavoriteItemIds(userId);
+
 			JSONArray itemList = new JSONArray();
 			for (Item item:items) {
-				itemList.put(item.toJSONObject());
+				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favoriteItems.contains(item.getItemID()));
+				itemList.put(obj);
 			}
 			RpcHelper.writeJsonArray(response, itemList);
 			
